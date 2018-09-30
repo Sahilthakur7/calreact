fd4c55de041378dea3f6b683bdfe74d3915d580f
SahilThakur7 <sahilrd07@gmail.com>
2018-09-30 12:07:47 +0530

    appointment model and form

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,4 +1,23 @@
 class AppointmentsController < ApplicationController
-  def index
-  end
+    def index
+        @appointment = Appointment.new
+        @appointments = Appointment.order('appointment_time DESC')
+    end
+
+    def create
+        @appointment = Appointment.new(appointment_params)
+        respond_to do |format|
+            if @appointment.save
+                @appointments = Appointment.order('appointment_time DESC')
+                format.js { }
+            end
+        end
+
+    end
+
+    private
+
+    def appointment_params
+        params.require(:appointment).permit(:title,:appointment_time)
+    end
 end
